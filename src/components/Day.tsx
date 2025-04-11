6e97d1d5606d473ed8b505600942359aeaffae63
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2025-04-12 01:55:02 +0530

    add empty schedule styling

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -24,10 +24,11 @@ function Day({ date }: DayProps) {
     <div
       key={date.toDateString()}
       id={`day-${date.toDateString()}`}
-      className="snap-start snap-always flex-shrink-0 md:flex-grow w-full md:w-auto h-full border-r last:border-r-0 bg-white">
-      <div className="w-full h-full flex items-center justify-center text-2xl font-semibold text-gray-700">
-        Content for {date.toDateString()}
-      </div>
+      className="flex items-center justify-center snap-start snap-always flex-shrink-0 md:flex-grow w-full md:w-auto h-full border-r last:border-r-0 bg-white p-2">
+      <p className="text-xs text-gray-500 font-normal text-center">
+        No schedule found for <br />
+        {date.toDateString()}
+      </p>
     </div>
   );
 }
