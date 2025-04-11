ebfe351817a46701d41c49903cc3081472d567b3
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2025-04-12 01:58:54 +0530

    handle phone styling for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,27 +10,27 @@ function Header() {
   return (
     <div className="sticky top-0 bg-white border-b z-10 px-4 py-3 flex items-center gap-2">
       <button
-        className="flex items-center justify-center text-sm md:text-base font-semibold py-2 px-4 hover:bg-gray-100 rounded-full transition-colors cursor-pointer border"
+        className="flex items-center justify-center text-sm md:text-base font-semibold py-1 px-2 md:py-2 md:px-4 hover:bg-gray-100 rounded-full transition-colors cursor-pointer border"
         onClick={handleTodayClick}
         aria-label="Today">
         Today
       </button>
 
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-1 md:gap-2">
         <button
-          className="flex w-8 h-8 md:w-10 md:h-10 items-center justify-center text-2xl p-2 hover:bg-gray-100 rounded-full transition-colors cursor-pointer"
+          className="flex w-8 h-8 md:w-10 md:h-10 items-center justify-center text-xl md:text-2xl p-2 hover:bg-gray-100 rounded-full transition-colors cursor-pointer"
           onClick={goToPreviousWeek}
           aria-label="Previous Week">
           ←
         </button>
         <button
-          className="flex w-10 h-10 items-center justify-center text-2xl p-2 hover:bg-gray-100 rounded-full transition-colors cursor-pointer"
+          className="flex w-8 h-8 md:w-10 md:h-10 items-center justify-center text-xl md:text-2xl p-2 hover:bg-gray-100 rounded-full transition-colors cursor-pointer"
           onClick={goToNextWeek}
           aria-label="Next Week">
           →
         </button>
       </div>
-      <h2 className="font-semibold text-xl md:text-2xl">
+      <h2 className="font-semibold text-xl md:text-2xl shrink-0">
         {month} {year}
       </h2>
     </div>
